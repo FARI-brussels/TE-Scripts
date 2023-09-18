61e049b60deba6ddf0537f4cbf4571c2ab337a71
mrcyme <simeonmcl@gmail.com>
2023-09-18 10:55:50 +0200

    swag

--- individual-demos/animalwelfare.sh
+++ individual-demos/animalwelfare.sh
@@ -2,30 +2,36 @@
 
 # Demo repo and demo directory path on the sbc here
 DEMO_ID="0"
-WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen-Fari_Internship"
+WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
+WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
 DEMO_REPO="https://github.com/FARI-brussels/demo-iridia-animal-welfare.git"
-DEMO_DIR="/home/fari/Documents/"
-DEMO_FOLDER="demo-iridia-animal-welfare"
+DEMO_DIR="/home/fari/Documents/demo-iridia-animal-welfare"
 
-cd "$WELCOME_SCREEN_DIR"
-git pull origin main
-#run welcome screen
+
+# Check if the welcome screen directory exists
+if [ -d "$WELCOME_SCREEN_DIR" ]; then
+  # If the directory exists, navigate to it and pull the latest from Git
+  cd "$WELCOME_SCREEN_DIR"
+  git pull origin main
+else
+  # If the directory doesn't exist, clone the repo into the directory
+  git clone "$WELCOME_SCREEN_REPO" "$WELCOME_SCREEN_DIR"
+fi
+
 gnome-terminal --working-directory=$WELCOME_SCREEN_DIR -- 'nohup python server.py' -T "Welcome Screen"
 
 # Check if the directory exists
-if [ -d "$DEMO_DIR/$DEMO_FOLDER" ]; then
+if [ -d "$DEMO_DIR" ]; then
   # If the directory exists, navigate to it and pull the latest from Git
   cd "$DEMO_DIR"
-  git pull origin master
+  git pull origin main
 else
   # If the directory doesn't exist, clone the repo into the directory
-  ECHO lol
-  git clone "$DEMO_REPO" "$DEMO_DIR/$DEMO_FOLDER"
-  cd "$DEMO_DIR"
+  git clone "$DEMO_REPO" "$DEMO_DIR"
 fi
 
 
 #run demo
-gnome-terminal --working-directory=$DEMO_DIR/$DEMO_FOLDER -- bash -c "nohup python -m flask --app=app --debug run --host=0.0.0.0"
+gnome-terminal --working-directory=$DEMO_DIR -- bash -c "nohup python -m flask --app=app --debug run --host=0.0.0.0"
 
 chromium-browser --kiosk "http://localhost:8080/$DEMO_ID"
