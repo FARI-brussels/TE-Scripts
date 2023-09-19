47c4c000a7c2ce28b07e4f81459a718f10ebd3e0
mrcyme <simeonmcl@gmail.com>
2023-09-19 11:52:55 +0200

    sbr

--- individual-demos/animalwelfare.sh
+++ individual-demos/animalwelfare.sh
@@ -8,35 +8,16 @@ WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
 DEMO_REPO="https://github.com/FARI-brussels/demo-iridia-animal-welfare.git"
 DEMO_DIR="/home/fari/Documents/demo-iridia-animal-welfare"
 
+# Assuming git_sync.sh and launch_demo.sh are in the same directory as this script
+SCRIPT_DIR="$(dirname "$0")"
 
-# Check if the welcome screen directory exists
-if [ -d "$WELCOME_SCREEN_DIR" ]; then
-  # If the directory exists, navigate to it and pull the latest from Git
-  cd "$WELCOME_SCREEN_DIR"
-  git pull origin main
-else
-  # If the directory doesn't exist, clone the repo into the directory
-  git clone "$WELCOME_SCREEN_REPO" "$WELCOME_SCREEN_DIR"
-fi
-#kill process on port 8080
-kill -9 $(lsof -t -i:8080)
-#remove chromium cache
-rm -rf ~/.cache/chromium
-#launch welcome screen
-gnome-terminal --working-directory=$WELCOME_SCREEN_DIR -- bash -c 'nohup python server.py' -T "Welcome Screen"
+# Use git_sync.sh to sync both repositories
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$DEMO_DIR" "$DEMO_REPO"
 
-# Check if the directory exists
-if [ -d "$DEMO_DIR" ]; then
-  # If the directory exists, navigate to it and pull the latest from Git
-  cd "$DEMO_DIR"
-  git pull origin main
-else
-  # If the directory doesn't exist, clone the repo into the directory
-  git clone "$DEMO_REPO" "$DEMO_DIR"
-fi
+# Launch the welcome screen using launch_welcome_screen.sh
+"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
 
 
 #run demo
 gnome-terminal --working-directory=$DEMO_DIR -- bash -c "nohup python -m flask --app=app --debug run --host=0.0.0.0"
-
-chromium-browser --kiosk "http://localhost:8080/$DEMO_ID"
