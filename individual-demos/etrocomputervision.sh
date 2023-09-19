9d0b694d84206fb5565d492806923f22aa3d6a11
mrcyme <simeonmcl@gmail.com>
2023-09-19 11:56:51 +0200

    sbr

--- individual-demos/etrocomputervision.sh
+++ individual-demos/etrocomputervision.sh
@@ -7,24 +7,17 @@ DEMO_REPO="git@github.com:FARI-brussels/demo-etro-visual-question-answering.git"
 DEMO_DIR="/home/fari/Documents/"
 DEMO_FOLDER="demo-etro-visual-question-answering"
 
-cd "$WELCOME_SCREEN_DIR"
-git pull origin main
-#run welcome screen
+# Assuming git_sync.sh and launch_demo.sh are in the same directory as this script
+SCRIPT_DIR="$(dirname "$0")"
+
+# Use git_sync.sh to sync both repositories
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$DEMO_DIR" "$DEMO_REPO"
+
+# Launch the welcome screen using launch_welcome_screen.sh
+"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
 
 
-# Check if the directory exists
-if [ -d "$DEMO_DIR/$DEMO_FOLDER" ]; then
-  # If the directory exists, navigate to it and pull the latest from Git
-  cd "$DEMO_DIR"
-  git pull origin master
-else
-  # If the directory doesn't exist, clone the repo into the directory
-  ECHO lol
-  git clone "$DEMO_REPO" "$DEMO_DIR/$DEMO_FOLDER"
-  cd "$DEMO_DIR"
-fi
-gnome-terminal --working-directory=$DEMO_DIR/$DEMO_FOLDER -e 'bash -c "nohup python3 main.py"' -T "Demo"
-gnome-terminal --working-directory=$WELCOME_SCREEN_DIR -e 'nohup python server.py' -T "Welcome Screen"
-chromium-browser --kiosk "http://localhost:8080/$DEMO_ID"
+gnome-terminal --working-directory=$DEMO_DIR/$DEMO_FOLDER -- bash -c "nohup python3 main.py"
 #run demo
 
