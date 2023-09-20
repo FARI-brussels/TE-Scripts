398464cf1ab9c39b20314fd114b9c0c43d683d0f
mrcyme <simeonmcl@gmail.com>
2023-09-20 10:51:58 +0200

    swage

--- individual-demos/ailabcomputervision.sh
+++ individual-demos/ailabcomputervision.sh
@@ -1,10 +1,15 @@
 
 DEMO_ID="2"
 WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen-Flet"
+WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/Welcome-Screen-Flet"
 
-cd "$WELCOME_SCREEN_DIR"
-git pull origin main
-#run welcome screen
+# Assuming git_sync.sh and launch_demo.sh are in the same directory as this script
+SCRIPT_DIR="$(dirname "$0")"
+
+# Use git_sync.sh to sync both repositories
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
+
+
 terminator --working-directory=$WELCOME_SCREEN_DIR -e "bash -c 'python main.py --id $DEMO_ID'" -T "Welcome Screen"
 pkill chromium-browser
 chromium-browser --kiosk "http://127.0.0.1:8550
