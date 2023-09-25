df87aea4cf3a3e49f3a0b4e7ac461d7c14004fd1
mrcyme <simeonmcl@gmail.com>
2023-09-25 15:17:52 +0200

    yes

---
+++ ssh_to_device.sh
@@ -0,0 +1,12 @@
+argument : 
+- mac adress
+- host name
+
+process : 
+- look up on the CMS for the ip corresponding to the mac adress
+- ping to that ip,
+ 
+ if the ip not accessible the run the update_ips script and look up cms for new ip 
+
+ssh to host name and ip.
+
---
+++ update_ips.sh
@@ -0,0 +1,5 @@
+https://www.pcwdld.com/find-device-or-ip-address-using-mac-address
+
+process : 
+1) retreive all mac adress from content management system
+2) 
