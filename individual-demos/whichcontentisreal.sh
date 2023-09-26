6e7b5a90d51f615d06d1ac22dc0d6245aa8d6267
mrcyme <simeonmcl@gmail.com>
2023-09-26 10:44:53 +0200

    swag

--- individual-demos/whichcontentisreal.sh
+++ individual-demos/whichcontentisreal.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
 # Demo repo and demo directory path on the sbc here
 DEMO_ID="12"
 WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
 WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
 DEMO_REPO="https://github.com/FARI-brussels/demo-fari-which-content-is-real.git"
 DEMO_DIR="/home/fari/Documents/demo-fari-which-content-is-real"
-SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
+
+
+# Assuming git_sync.sh and launch_demo.sh are in the same directory as this script
+SCRIPT_DIR="$(dirname "$0")"
 
 # Use git_sync.sh to sync both repositories
 "$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
@@ -20,3 +22,4 @@ SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 #kill process on port 8000
 kill -9 $(lsof -t -i:8000)
 gnome-terminal --working-directory=$DEMO_DIR/$DEMO_FOLDER -- bash -c "nohup python server.py" 
+>>>>>>> 8b9b733 (unix fix)
