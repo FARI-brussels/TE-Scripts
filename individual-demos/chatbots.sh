0f0aca05acff4c7f3db3c100b6bd87ecb25b7ed9
mrcyme <simeonmcl@gmail.com>
2023-09-26 09:48:56 +0200

    script-dir

--- individual-demos/chatbots.sh
+++ individual-demos/chatbots.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Demo repo and demo directory path on the sbc here
 DEMO_ID="13"
 WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
@@ -7,10 +6,6 @@ WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
 DEMO_REPO="https://github.com/ShadhviVR/Chatbot-fari.git"
 DEMO_DIR="/home/fari/Documents/demo-fari-chatbot-frontend"
 SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
-
-
-# Assuming git_sync.sh and launch_demo.sh are in the same directory as this script
-
 
 # Use git_sync.sh to sync both repositories
 "$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
