51a5c163f2a4f6c241140355ca96e3605054d5c1
mrcyme <simeonmcl@gmail.com>
2023-12-06 14:49:32 +0100

    swarm

---
+++ individual-demos/swarmrobotics.sh
@@ -0,0 +1,10 @@
+WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
+WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
+DEMO_ID="13"
+SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
+
+# Use git_sync.sh to sync both repositories
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
+
+# Launch the welcome screen using launch_welcome_screen.sh
+"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
