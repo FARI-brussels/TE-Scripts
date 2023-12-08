c2158008d0031dd5cf683e5c674432b2472a528b
mrcyme <simeonmcl@gmail.com>
2023-12-08 10:23:13 +0100

    tictactoe demo

---
+++ individual-demos/tictactoe.sh
@@ -0,0 +1,19 @@
+DEMO_ID="15"
+WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
+WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
+DEMO_REPO="https://github.com/FARI-brussels/demo-iridia-swarm-robotics.git"
+DEMO_DIR="/home/fari/Documents/demo-iridia-swarm-robotics"
+SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
+
+# Use git_sync.sh to sync both repositories
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$DEMO_DIR" "$DEMO_REPO"
+
+# Launch the welcome screen using launch_welcome_screen.sh
+"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
+
+#run demo
+gnome-terminal --working-directory=$DEMO_DIR python main.py
+#kill process on port 5000
+kill -9 $(lsof -t -i:5000)
+gnome-terminal --working-directory=$DEMO_DIR/frontend -- bash -c "nohup python server.py"
