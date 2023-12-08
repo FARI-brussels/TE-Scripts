c75fc9428ac03d1fe8d696a50260aa98b53cddaa
mrcyme <simeonmcl@gmail.com>
2023-12-08 10:45:43 +0100

    tictactoe demo

--- individual-demos/tictactoe.sh
+++ individual-demos/tictactoe.sh
@@ -1,8 +1,8 @@
 DEMO_ID="15"
 WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
 WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
-DEMO_REPO="https://github.com/FARI-brussels/demo-iridia-swarm-robotics.git"
-DEMO_DIR="/home/fari/Documents/demo-iridia-swarm-robotics"
+DEMO_REPO="https://github.com/FARI-brussels/demo-fari-robotic-arm"
+DEMO_DIR="/home/fari/Documents/demo-fari-robotic-arm"
 SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 
 # Use git_sync.sh to sync both repositories
@@ -13,7 +13,9 @@ SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 "$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
 
 #run demo
-gnome-terminal --working-directory=$DEMO_DIR python main.py
+gnome-terminal --working-directory=$DEMO_DIR -- bash -c "source /home/fari/anaconda3/etc/profile.d/conda.sh && conda activate demo-fari-robotic-arm && python main.py; read -p 'Press enter to continue...'"
+
 #kill process on port 5000
 kill -9 $(lsof -t -i:5000)
 gnome-terminal --working-directory=$DEMO_DIR/frontend -- bash -c "nohup python server.py"
+
