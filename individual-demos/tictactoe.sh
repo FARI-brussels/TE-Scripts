410615cb23174a120a082b49aac6e6aad57e7513
Fari <experience@fari.brussels>
2024-01-17 12:27:01 +0100

    tictactoe demo

--- individual-demos/tictactoe.sh
+++ individual-demos/tictactoe.sh
@@ -9,13 +9,19 @@ SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 "$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
 "$SCRIPT_DIR/clone_or_pull_repo.sh" "$DEMO_DIR" "$DEMO_REPO"
 
-# Launch the welcome screen using launch_welcome_screen.sh
-"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
-
 #run demo
 gnome-terminal --working-directory=$DEMO_DIR -- bash -c "source /home/fari/anaconda3/etc/profile.d/conda.sh && conda activate demo-fari-robotic-arm && python main.py; read -p 'Press enter to continue...'"
+
 
 #kill process on port 5000
 kill -9 $(lsof -t -i:5000)
 gnome-terminal --working-directory=$DEMO_DIR/frontend -- bash -c "nohup python server.py"
+
+sleep 5
+
+# Launch the welcome screen using launch_welcome_screen.sh
+"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
+
+
+
 
