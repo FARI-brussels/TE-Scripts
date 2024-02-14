9441b11da3af63b9d7cfcb5d645c379a3ed54d43
mrcyme <simeonmcl@gmail.com>
2024-02-14 14:20:40 +0100

    Add totem.sh script for launching welcome screen and syncing repositories

---
+++ totem/totem.sh
@@ -0,0 +1,18 @@
+DEMO_ID="3"
+TOTEM_INTERFACE_DIR="/home/fari/Documents/Totem-Interface"
+TOTEM_INTERFACE_REPO="https://github.com/FARI-brussels/Totem-Interface"
+SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
+
+# Use git_sync.sh to sync both repositories
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
+
+# Launch the welcome screen using launch_welcome_screen.sh
+# Kill process on port 8080
+kill -9 $(lsof -t -i:8080)
+
+# Remove chromium cache
+rm -rf ~/.cache/chromium
+
+# Launch welcome screen in a new gnome terminal
+gnome-terminal --working-directory=$TOTEM_INTERFACE_DIR -- bash -c 'nohup python -m http.server 8080'
+chromium-browser --kiosk "http://localhost:8080" 
