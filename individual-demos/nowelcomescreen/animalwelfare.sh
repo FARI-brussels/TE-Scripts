6fb84cedda30289bdf4a0421afe065d38ac05815
mrcyme <simeonmcl@gmail.com>
2024-03-13 14:38:35 +0100

    Add animal no welcomescreen welfare and simplex demos

---
+++ individual-demos/nowelcomescreen/animalwelfare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DEMO_REPO="https://github.com/FARI-brussels/demo-iridia-animal-welfare.git"
+DEMO_DIR="/home/fari/Documents/demo-iridia-animal-welfare"
+SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
+
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$DEMO_DIR" "$DEMO_REPO"
+
+
+#run demo
+gnome-terminal --working-directory=$DEMO_DIR -- bash -c "nohup python -m flask --app=app --debug run --host=0.0.0.0"
+chromium-browser --kiosk "http://localhost:8000" &
+
+sleep 20
+#press escape for exiting menu in gnome (the menu mode is lauched on startup)
+xdotool key Escape
