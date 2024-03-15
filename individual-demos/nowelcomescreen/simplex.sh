a9acf386dc4d62e035a62902bbbd86090106ab08
mrcyme <simeonmcl@gmail.com>
2024-03-15 09:20:04 +0100

    Update welcome screen URL to simplex.smartlawhub.eu

--- individual-demos/nowelcomescreen/simplex.sh
+++ individual-demos/nowelcomescreen/simplex.sh
@@ -1,12 +1,3 @@
-DEMO_ID="3"
-WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
-WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
-SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
-
-# Use git_sync.sh to sync both repositories
-"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
-
-# Launch the welcome screen using launch_welcome_screen.sh
-"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
+chromium --kiosk https://simplex.smartlawhub.eu/
 
 
