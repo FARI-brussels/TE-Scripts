2152fdfa87745d427d1a888b8969d611501ba843
mrcyme <simeonmcl@gmail.com>
2024-03-18 15:39:22 +0100

    Update launch_welcome_screen.sh and totem/display_screen.sh

--- launch_welcome_screen.sh
+++ launch_welcome_screen.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-# Check if both arguments were passed
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 WELCOME_SCREEN_DIR DEMO_ID"
+# Check if the number of arguments is less than 2
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 WELCOME_SCREEN_DIR DEMO_ID [BROWSER]"
     exit 1
 fi
 
 WELCOME_SCREEN_PATH="$1"
 DEMO_ID="$2"
+BROWSER=${3:-chromium} # Default to chromium if not specified
 
 # Kill process on port 8080
 kill -9 $(lsof -t -i:8080)
 
-# Remove chromium cache
-rm -rf ~/.cache/chromium
+# Check which browser to use and remove its cache
+if [ "$BROWSER" = "firefox" ]; then
+    CACHE_DIR=~/.cache/mozilla/firefox
+else
+    CACHE_DIR=~/.cache/chromium
+fi
+
+rm -rf "$CACHE_DIR"
 
 # Launch welcome screen in a new gnome terminal
-gnome-terminal --working-directory=$WELCOME_SCREEN_PATH -- bash -c 'nohup python server.py'
-chromium-browser --kiosk "http://localhost:8080/$DEMO_ID" &
+gnome-terminal --working-directory="$WELCOME_SCREEN_PATH" -- bash -c 'nohup python server.py &'
+
+# Launch the specified browser
+if [ "$BROWSER" = "firefox" ]; then
+    firefox --kiosk "http://localhost:8080/$DEMO_ID" &
+else
+    chromium-browser --kiosk "http://localhost:8080/$DEMO_ID" &
+fi
+
 
 
 
--- totem/display_screen.sh
+++ totem/display_screen.sh
@@ -1,4 +1,4 @@
 sleep 5
 xdotool key Escape
 sleep 5
-vlc --fullscreen --loop /home/fari/TE-Scripts/totem/ftyslideshow.mp4
+mpv --fullscreen --loop /home/fari/TE-Scripts/totem/ftyslideshow.mp4
