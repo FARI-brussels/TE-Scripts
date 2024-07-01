e2e9d00e3412cf8a339ae0295a51630df92321c6
YFari-brussels <experience@fari.brussels>
2024-07-01 09:51:04 +0200

    tictactoe

--- individual-demos/tictactoe.sh
+++ individual-demos/tictactoe.sh
@@ -1,27 +1,25 @@
-DEMO_ID="15"
-WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen"
-WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/welcome-screen"
-DEMO_REPO="https://github.com/FARI-brussels/demo-fari-robotic-arm"
-DEMO_DIR="/home/fari/Documents/demo-fari-robotic-arm"
+BACKEND_REPO="https://github.com/FARI-brussels/demo-fari-tic-tac-toe-backend"
+FRONTEND_REPO="git@github.com:FARI-brussels/demo-fari-tic-tac-toe-frontend.git"
+BACKEND_DIR="/home/fari/Documents/demo-fari-tic-tac-toe-backend"
+FRONTEND_DIR="/home/fari/Documents/demo-fari-tic-tac-toe-frontend"
 SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 
 # Use git_sync.sh to sync both repositories
-"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
-"$SCRIPT_DIR/clone_or_pull_repo.sh" "$DEMO_DIR" "$DEMO_REPO"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$BACKEND_DIR" "$BACKEND_REPO"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$FRONTEND_DIR" "$FRONTEND_REPO"
 
-#run demo
-gnome-terminal --working-directory=$DEMO_DIR -- bash -c "source /home/fari/anaconda3/etc/profile.d/conda.sh && conda activate demo-fari-robotic-arm && python main.py; read -p 'Press enter to continue...'"
+#run backend
+gnome-terminal --working-directory=$BACKEND_DIR -- bash -c "source /home/fari/miniconda3/etc/profile.d/conda.sh && conda activate tictactoe && python main.py --modes REAL; read -p 'Press enter to continue...'"
+
 
 
 #kill process on port 5000
 kill -9 $(lsof -t -i:5000)
-gnome-terminal --working-directory=$DEMO_DIR/frontend -- bash -c "nohup python server.py"
 
-sleep 5
+#run frontend
+gnome-terminal --working-directory=$FRONTEND_DIR -- bash -c "pnpm dev; read -p 'Press enter to continue...'"
 
-# Launch the welcome screen using launch_welcome_screen.sh
-"$SCRIPT_DIR/launch_welcome_screen.sh" "$WELCOME_SCREEN_DIR" "$DEMO_ID"
-
+gnome-terminal --working-directory=$FRONTEND_DIR -- bash -c "pnpm backend:dev; read -p 'Press enter to continue...'"
 
 
-
+chromium-browser --kiosk "http://localhost:5173" &
