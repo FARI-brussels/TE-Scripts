13987793389e7d310f16d0caef46bc36c5d46f8a
mrcyme <simeonmcl@gmail.com>
2024-07-23 17:19:56 +0200

    added guesswhoPoc

--- individual-demos/guesswhoPOC.sh
+++ individual-demos/guesswhoPOC.sh
@@ -19,5 +19,5 @@ kill -9 $(lsof -t -i:5000)
 gnome-terminal --working-directory=$DEMO_DIR -- bash -c "python app.py; echo 'Press Enter to exit'; read"
 gnome-terminal --working-directory=$DEMO_DIR -- bash -c "python websocket_server.py; echo 'Press Enter to exit'; read"
 
-gnome-terminal -- bash -c "DISPLAY=:1 chromium-browser "http://localhost:5000" &"
-gnome-terminal -- bash -c "DISPLAY=:0 chromium-browser "http://localhost:5000/robot_view" &"
+gnome-terminal -- bash -c "DISPLAY=:1 chromium-browser 'http://localhost:5000' 'Press Enter to exit'; read"
+gnome-terminal -- bash -c "DISPLAY=:0 chromium-browser 'http://localhost:5000/robot_view' 'Press Enter to exit'; read"
