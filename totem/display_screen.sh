c017668df0107c1d42c35fc6bb8bbe8588eaf125
mrcyme <simeonmcl@gmail.com>
2024-11-29 14:10:20 +0100

    no conf

--- totem/display_screen.sh
+++ totem/display_screen.sh
@@ -4,5 +4,5 @@ done
 export DISPLAY=:0
 sleep 10 
 gnome-terminal -- bash -c "xdotool key Escape; echo 'Press Enter to exit'; read"
-mpv --fullscreen --loop /home/fari/Documents/TE-Scripts/totem/conference.mp4
+mpv --fullscreen --loop /home/fari/Documents/TE-Scripts/totem/welcome.mp4
 
