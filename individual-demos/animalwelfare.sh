559050891413bf83bb345a5fe06416269e0b673c
m0satron <msebdani@gmail.com>
2025-01-28 13:59:47 +0100

    fix(animal welfare): clone and run backend scripts

--- individual-demos/animalwelfare.sh
+++ individual-demos/animalwelfare.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Demo repo and demo directory path on the sbc here
-DEMO_REPO="https://github.com/FARI-brussels/Welcome-Screen-v2.git"
-DEMO_DIR="/home/fari/Documents/Welcome-Screen-v2"
+FRONTEND_REPO="https://github.com/FARI-brussels/Welcome-Screen-v2.git"
+FRONTEND_DIR="/home/fari/Documents/Welcome-Screen-v2"
+
+BACKEND_REPO="https://github.com/FARI-brussels/demo-iridia-animal-welfare"
+BACKEND_DIR="/home/fari/Documents/demo-iridia-animal-welfare"
+
 SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 
 # Clone or pull the latest version of the repository
-"$SCRIPT_DIR/clone_or_pull_repo.sh" "$DEMO_DIR" "$DEMO_REPO"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$BACKEND_DIR" "$BACKEND_REPO"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$FRONTEND_DIR" "$FRONTEND_REPO"
 
 # Set the correct Node.js version using nvm
 export NVM_DIR="$HOME/.nvm"
@@ -23,17 +28,17 @@ rm -rf ~/.cache/chromium
 kill -9 $(lsof -t -i:5173)
 
 # Navigate to the demo directory and run npm install
-cd "$DEMO_DIR"
+cd "$FRONTEND_DIR"
 npm install
 
 # Launch welcome screen in a new gnome terminal after npm install completes
-gnome-terminal --working-directory=$DEMO_DIR -- bash -c "npm run demo --slug=animal-welfare; echo 'Press Enter to exit'; read"
+gnome-terminal --working-directory=$FRONTEND_DIR -- bash -c "npm run demo --slug=animal-welfare; echo 'Press Enter to exit'; read"
 
 # Open Chromium in kiosk mode
 gnome-terminal -- bash -c 'chromium-browser --kiosk "http://localhost:5173"'
 
 #run demo
-gnome-terminal --working-directory=$DEMO_DIR -- bash -c "flask run; echo 'Press exit to enter'; read"
+gnome-terminal --working-directory=$BACKEND_DIR -- bash -c "flask run; echo 'Press exit to enter'; read"
 
 # Wait for the system to initialize (sleep for 20 seconds)
 sleep 5
