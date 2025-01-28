cd8a3f5826fcb0fa340431d84630381683e6a90c
m0satron <msebdani@gmail.com>
2025-01-28 14:23:39 +0100

    feat(Hybrid AI): update script to use v2

--- individual-demos/ailabcomputervision.sh
+++ individual-demos/ailabcomputervision.sh
@@ -1,14 +1,40 @@
+#!/bin/bash
+# Demo repo and demo directory path on the sbc here
+FRONTEND_REPO="https://github.com/FARI-brussels/Welcome-Screen-v2.git"
+FRONTEND_DIR="/home/fari/Documents/Welcome-Screen-v2"
 
-DEMO_ID="2"
-WELCOME_SCREEN_DIR="/home/fari/Documents/Welcome-Screen-Flet"
-WELCOME_SCREEN_REPO="https://github.com/FARI-brussels/Welcome-Screen-Flet"
 SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 
-# Use git_sync.sh to sync both repositories
-"$SCRIPT_DIR/clone_or_pull_repo.sh" "$WELCOME_SCREEN_DIR" "$WELCOME_SCREEN_REPO"
+# Clone or pull the latest version of the repository
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$FRONTEND_DIR" "$FRONTEND_REPO"
+
+# Set the correct Node.js version using nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Use a specific Node.js version
+nvm use node
+
+# Remove chromium cache
+rm -rf ~/.cache/chromium
+
+# Kill any process using port 5173 (if running)
+kill -9 $(lsof -t -i:5173)
+
+# Navigate to the demo directory and run npm install
+cd "$FRONTEND_DIR"
+npm install
+
+# Launch welcome screen in a new gnome terminal after npm install completes
+gnome-terminal --working-directory=$FRONTEND_DIR -- bash -c "npm run demo --slug=hybrid-ai-for-image-understanding; echo 'Press Enter to exit'; read"
+
+# Open Chromium in kiosk mode
+gnome-terminal -- bash -c 'chromium-browser --kiosk "http://localhost:5173"'
 
 
-xfce4-terminal --working-directory=$WELCOME_SCREEN_DIR -e "bash -c 'python main.py --id $DEMO_ID'" &
-echo lol
-killall chromium-browser
-chromium-browser --kiosk "http://127.0.0.1:8550"
+# Wait for the system to initialize (sleep for 20 seconds)
+sleep 5
+
+# Press Escape to exit the menu in Gnome (if needed)
+xdotool key Escape
