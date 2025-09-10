687f298029860333a3f564655ea159f2a62b37ea
mrcyme <simeonmcl@gmail.com>
2025-09-10 14:13:52 +0200

    heatmap

--- individual-demos/legoheatmap.sh
+++ individual-demos/legoheatmap.sh
@@ -2,7 +2,8 @@
 # Demo repo and demo directory path on the sbc here
 REPO="https://github.com/FARI-brussels/demo-etro-heat-island.git"
 DIR="/home/fari/Documents/demo-etro-heat-island"
-
+FRONTEND_DIR="$DIR/frontend"
+BACKEND_DIR="$DIR/backend"
 SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 # Use git_sync.sh to sync both repositories
 "$SCRIPT_DIR/clone_or_pull_repo.sh" "$DIR" "$REPO"
@@ -14,17 +15,17 @@ rm -rf ~/.cache/chromium
 kill -9 $(lsof -t -i:5173)
 
 # Navigate to the demo directory and run npm install
-cd "$DIR/frontend"
+cd $FRONTEND_DIR
 npm install
 
 #run backend
 #gnome-terminal --working-directory=$BACKEND_DIR -- bash -c "source /home/fari/miniconda3/etc/profile.d/conda.sh && conda activate tictactoe && python main.py --modes REAL;"
 
-gnome-terminal --working-directory="$DIR/backend" -- bash -c "python app.py; read -p 'Press enter to continue...'"
+gnome-terminal --working-directory=$FRONTEND_DIR -- bash -c "python app.py; read -p 'Press enter to continue...'"
 
 
 #run frontend
-gnome-terminal --working-directory="$DIR/frontend" -- bash -c "npm run dev; read -p 'Press enter to continue...'"
+gnome-terminal --working-directory=$BACKEND_DIR -- bash -c "npm run dev; read -p 'Press enter to continue...'"
 
 gnome-terminal -- bash -c "firefox --kiosk 'http://localhost:5173'; read -p 'Press enter to continue...'"
 
