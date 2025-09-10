637a83dbaf454b7992f6ae82f9694cad0e27ed2a
mrcyme <simeonmcl@gmail.com>
2025-09-10 13:55:30 +0200

    heatmap

--- individual-demos/legoheatmap.sh
+++ individual-demos/legoheatmap.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # Demo repo and demo directory path on the sbc here
+REPO="https://github.com/FARI-brussels/demo-etro-heat-island.git"
+DIR="/home/fari/Documents/demo-heatmap-poc/backend"
 
-
-BACKEND_REPO="https://github.com/FARI-brussels/demo-heatmap-poc.git"
-FRONTEND_REPO="https://github.com/FARI-brussels/demo-heatmap-poc.git"
-BACKEND_DIR="/home/fari/Documents/demo-heatmap-poc/backend"
-FRONTEND_DIR="/home/fari/Documents/demo-heatmap-poc/frontend"
 SCRIPT_DIR="/home/fari/Documents/TE-Scripts"
 # Use git_sync.sh to sync both repositories
-"$SCRIPT_DIR/clone_or_pull_repo.sh" "$BACKEND_DIR" "/home/fari/Documents/demo-heatmap-poc/"
+"$SCRIPT_DIR/clone_or_pull_repo.sh" "$DIR" "$REPO"
 
 # Remove chromium cache
 rm -rf ~/.cache/chromium
@@ -17,17 +14,17 @@ rm -rf ~/.cache/chromium
 kill -9 $(lsof -t -i:5173)
 
 # Navigate to the demo directory and run npm install
-cd "$FRONTEND_DIR"
+cd "$DIR/frontend"
 npm install
 
 #run backend
 #gnome-terminal --working-directory=$BACKEND_DIR -- bash -c "source /home/fari/miniconda3/etc/profile.d/conda.sh && conda activate tictactoe && python main.py --modes REAL;"
 
-gnome-terminal --working-directory=$BACKEND_DIR -- bash -c "python app.py; read -p 'Press enter to continue...'"
+gnome-terminal --working-directory="$DIR/backend" -- bash -c "python app.py; read -p 'Press enter to continue...'"
 
 
 #run frontend
-gnome-terminal --working-directory=$FRONTEND_DIR -- bash -c "npm run dev; read -p 'Press enter to continue...'"
+gnome-terminal --working-directory="$DIR/frontend" -- bash -c "npm run dev; read -p 'Press enter to continue...'"
 
 gnome-terminal -- bash -c "firefox --kiosk 'http://localhost:5173'; read -p 'Press enter to continue...'"
 
