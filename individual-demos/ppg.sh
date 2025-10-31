ba2ad2069a265e491001ca2f2b509d650353064c
mrcyme <simeonmcl@gmail.com>
2025-10-31 11:43:08 +0100

    ppg

--- individual-demos/ppg.sh
+++ individual-demos/ppg.sh
@@ -29,7 +29,7 @@ npm install
 gnome-terminal --working-directory=$DEMO_DIR -- bash -c "npm run dev; echo 'Press Enter to exit'; read"
 
 # Open Chromium in kiosk mode
-gnome-terminal -- bash -c 'chromium-browser --kiosk "http://localhost:5173"'
+gnome-terminal -- bash -c 'google-chrome --kiosk "http://localhost:5173"'
 
 # Wait for the system to initialize (sleep for 20 seconds)
 sleep 5
